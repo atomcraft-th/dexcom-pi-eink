bd27f02e711519cf7cb819330f7b170257e50a70
Tom Hollis <tom.hollis@accelercomm.com>
2023-11-30 17:09:13 +0000

    give path to python in bash script

--- run_dexcom_pi_eink.sh
+++ run_dexcom_pi_eink.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 while true
 do
-	python3 dexcom_pi_eink.py
+	python3 ${SCRIPT_DIR}/dexcom_pi_eink.py
 	sleep 30
 done
